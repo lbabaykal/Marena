38b45256be973015c392e41e33d00ba079db5803
IBaBaYKAI <mei.babayka@gmail.com>
2023-10-25 22:11:35 +0300

    Изменение role_id с 3 на 2 по умолчанию.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->unsignedBigInteger('role_id')->default(3);
+            $table->unsignedBigInteger('role_id')->default(2);
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();

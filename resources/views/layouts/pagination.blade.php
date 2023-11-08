686b71326f7f323f909f64fc2a5a6367ffffb911
IBaBaYKAI <mei.babayka@gmail.com>
2023-11-09 00:06:47 +0300

    перевод кнопок пагинации которые недоступны.

--- resources/views/layouts/pagination.blade.php
+++ resources/views/layouts/pagination.blade.php
@@ -3,7 +3,7 @@
         <ul>
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
-                <a class="pagination-previous-none" href="javascript:void(0);"><li>Previous</li></a>
+                <a class="pagination-previous-none" href="javascript:void(0);"><li>Сюда</li></a>
             @else
                 <a class="pagination-previous" href="{{ $paginator->previousPageUrl() }}"><li>Сюда</li></a>
             @endif
@@ -31,7 +31,7 @@
             @if ($paginator->hasMorePages())
                 <a class="pagination-next" href="{{ $paginator->nextPageUrl() }}"><li>Туда</li></a>
             @else
-                <a class="pagination-next-none" href="javascript:void(0);"><li>Previous</li></a>
+                <a class="pagination-next-none" href="javascript:void(0);"><li>Туда</li></a>
             @endif
         </ul>
     </div>
